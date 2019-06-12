b6b567cacb2b62a846e60528ac450f81d3a37c03
Gordon <gordonzhou68@outlook.com>
2019-06-12 19:28:14 +0000

    created view files for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,23 +1,27 @@
 class Admin::CategoriesController < ApplicationController
-
-  def index
-    @categories = Category.all.order(created_at: :desc)
-  end
+      def index
+        @categories = Category.all
+      end
     
-  def new
-    @category = Category.new
-  end
+      def new
+        @category = Category.new
+      end
     
-  def create
-    @category = Category.new(category_params)
-  end
+      def create
+        @category = Category.new(category_params)
+    
+        if @category.save
+          redirect_to [:admin, :categories], notice: 'Category created!'
+        else
+          render :new
+        end
+      end
 
-  private
+      private
 
-  def category_params
-    params.require(:category).permit(
-      :name
-    )
-  end
-     
+      def category_params
+        params.require(:category).permit(
+          :name
+        )
+      end
 end
