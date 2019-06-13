fbea2077dadbd67aa8106d70dc52309f10a4cd22
Gordon <gordonzhou68@outlook.com>
2019-06-13 00:02:51 +0000

    user authentification feature implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,17 @@
 class UsersController < ApplicationController
+    
     def new
     end
 
     def create
-        user = User.find_by_email(params[:email])
-        # If the user exists AND the password entered is correct.
-        if user && user.authenticate(params[:password])
-          # Save the user id inside the browser cookie. This is how we keep the user 
-          # logged in when they navigate around our website.
+        user = User.new(user_params)
+        if user.save
           session[:user_id] = user.id
           redirect_to '/'
         else
-        # If user's login doesn't work, send them back to the login form.
-          redirect_to '/login'
+          redirect_to '/signup'
         end
-    end
-    
-    def destroy
-        session[:user_id] = nil
-        redirect_to '/login'
-    end
+    end 
 
     private
 
