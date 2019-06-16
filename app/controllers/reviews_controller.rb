cea712617d67387c853787282086a6afecff5583
Gordon <gordonzhou68@outlook.com>
2019-06-16 20:40:25 +0000

    product ratings feature implemented

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,31 @@
 class ReviewsController < ApplicationController
-    def review_params
-        params.require(:review).permit(:description, :rating)
-    end
+
+    before_filter :authorize
+
     def create
         #find the parent element
         @product = Product.find(params[:product_id])
         @review = @product.reviews.create(review_params)
         @review.user = current_user
         if (@review.save!)
-            redirect_to products_path
+            redirect_to @product
         else
             redirect_to product_path
         end
     end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        @product = Product.find(params[:product_id])
+        flash[:success] = "Review deleted"
+        redirect_to @product
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:description, :rating)
+    end
+    
 end
